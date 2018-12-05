c622ab518e629474a4bf573410ea74ae098b9525
ChristopheRibault <c.ribault.dev@gmail.com>
2018-12-05 15:20:21 +0100

    add dateformat

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,4 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import dateFormat from 'dateformat';
+
+dateFormat.i18n = {
+  dayNames: [
+      'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam',
+      'dimanch', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'
+  ],
+  monthNames: [
+      'Jan', 'Feb', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aut', 'Sep', 'Oct', 'Nov', 'Dec',
+      'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+  ],
+  timeNames: [
+      'a', 'p', 'am', 'pm', 'A', 'P', 'AM', 'PM'
+  ]
+};
 
 class Offer extends Component {
   render() {
@@ -7,7 +22,7 @@ class Offer extends Component {
       <div className='Offer'>
         <img src ='' className='logo' alt='logo' />
         <div className='content'>
-          <h3>{`${data.title} | ${data.contract_type} | ${data.updated_at}`}</h3>
+          <h3>{`${data.title} | ${data.contract_type} | ${dateFormat(data.updated_at, 'dd mmmm yyyy')}`}</h3>
           <p>{data.description}</p>
         </div>
       </div>
