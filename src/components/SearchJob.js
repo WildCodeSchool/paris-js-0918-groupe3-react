1f7d980a542aa796bb392a7f88355cddc67665d0
D0C702WH0 <jemyplu@gmail.com>
2018-12-05 08:22:58 +0100

    search barre poste

---
+++ src/components/SearchJob.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class SearchJob extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+export default SearchJob;
