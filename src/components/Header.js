9752714a887d5272554c3d554f743b99f48dc3fd
olivier chabot <chabot.oliv@gmail.com>
2018-12-06 17:16:38 +0100

    maj header

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import logo from "../images/Logo.png";
+
+import "./css/header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+          <a className="navbar-brand" href="/">
+            <img className="logo" src={logo} alt="Logo" />
+            &nbsp; &nbsp;
+            <h2 className="d-inline align-middle">Dessine-moi un job</h2>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+          <button type="button" className="btn btnMenuBurger d-lg-none m-2">Espace candidat</button>
+          <button type="button" className="btn btnMenuBurger d-lg-none m-2">Espace recruteur</button>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Accueil <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="divider-vertical d-none d-lg-block" />
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Pourquoi ?
+                </a>
+              </li>
+              <li className="divider-vertical d-none d-lg-block" />
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Comment postuler ?
+                </a>
+              </li>
+              <div className="btn-group dropleft ml-3 d-none d-lg-block">
+                <button
+                  type="button"
+                  className="btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Connexion
+                </button>
+                <div className="dropdown-menu">
+                  <a className="dropdown-item" href="/">
+                    Espace candidat
+                  </a>
+                  <div className="dropdown-divider" />
+                  <a className="dropdown-item" href="/">
+                    Espace recruteur
+                  </a>
+                </div>
+              </div>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
