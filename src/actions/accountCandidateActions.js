4eaf6da587ce8ce3d41fcb4d31f6e45183f0e947
FabienTS <f.thalabas@gmail.com>
2019-01-15 17:46:24 +0100

    affichage des reponses du candidat quand il clique sur une annonce où il a postulé

--- src/actions/accountCandidateActions.js
+++ src/actions/accountCandidateActions.js
@@ -1,4 +1,4 @@
-import { GET_CANDIDATE_INFO } from "./types";
+import { GET_CANDIDATE_INFO, GET_INFO_OFFERS_APPLICATION, GET_ANSWER_CANDIDATE} from "./types";
 import axios from "axios";
 
 const domain = process.env.REACT_APP_DOMAIN_NAME;
@@ -17,6 +17,40 @@ export const getCandidateInfo = () => dispatch => {
     dispatch({
       type: GET_CANDIDATE_INFO,
       candidateInfo: res.data[0]
+    });
+  });
+};
+
+export const getInfoOffersApplication = () => dispatch => {
+  const url = `${domain}api/candidates/applications`;
+
+  axios({
+    method: "GET",
+    url,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(res => {
+    dispatch({
+      type: GET_INFO_OFFERS_APPLICATION,
+      getInfoOffers: res.data
+    });
+  });
+};
+
+export const getAnswerCandidate = (idOffer) => dispatch => {
+  const url = `${domain}api/candidates/offer${idOffer}/answers`;
+
+  axios({
+    method: "GET",
+    url,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(res => {
+    dispatch({
+      type: GET_ANSWER_CANDIDATE,
+      getAnswerCandidate: res.data
     });
   });
 };
---
+++ src/components/AnswersCandidate.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { getAnswerCandidate } from "../actions/accountCandidateActions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import OrangeButton from "./OrangeButton";
+
+class AnswersCandidate extends Component {
+    componentDidMount = () => {
+            this.props.getAnswerCandidate(this.props.match.params.idOffer)
+        }
+    render () {
+        const { answersCandidate } = this.props
+        console.log("reponse candidat", answersCandidate)
+        return(
+            <div className="AnswersCandidate">
+                {answersCandidate.map((e, i) => (
+                    <div key={`answers-${i}`}> 
+                        <p>{`${i+1}. ${e.question}`}</p>
+                        <p>{e.answer}</p>
+                    </div>
+
+                ))}
+                <Link to='/candidates'><OrangeButton text='Retour à mon espace'/></Link>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    answersCandidate: state.accountCandidate.answersCandidate
+  });
+  
+  export default connect(
+    mapStateToProps,
+    { getAnswerCandidate }
+  )(AnswersCandidate);
