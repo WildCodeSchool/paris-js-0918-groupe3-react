1c78733c48c689d9545b5629d8115dfdbdc62aad
ThibltC <yojibo@hotmail.fr>
2019-01-16 10:12:58 +0100

    ajout du bouton pour revenir dans le compte companie quand on est sur la page des cadidatures dune offre

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { getApplicationsOnOffers } from "../actions/accountCompanyActions";
 import sortApplicationsByCandidate from "../helpers/sortApplicationsByCandidate";
 
 
 import ApplicationCompany from "./ApplicationCompany"
-
+import OrangeButton from './OrangeButton';
 
 
 class Applications extends Component {
-
 
     componentDidMount = () => {
         this.props.getApplicationsOnOffers(this.props.match.params.id)
     }
 
-
     render() {
         const applicationById = sortApplicationsByCandidate(this.props.applicationsCompany)
-        
         return (
             <div className='Applications'>
-
-
+                <Link to='/companies'><OrangeButton text="Retour" /></Link>
                 {applicationById.map((e, i) =>
-                    <ApplicationCompany index={i} element={e}/>
+                    <ApplicationCompany index={i} element={e} />
                 )}
-
-
-
-
             </div>
         )
     }
