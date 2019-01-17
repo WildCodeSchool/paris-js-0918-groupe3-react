41d835a93f6c1a889461d27eb0e21d625179396b
D0C702WH0 <jemyplu@gmail.com>
2019-01-17 20:41:51 +0100

    correction search tous error

--- src/actions/searchOffersActions.js
+++ src/actions/searchOffersActions.js
@@ -5,22 +5,22 @@ const domain = process.env.REACT_APP_DOMAIN_NAME;
 
 export const searchOffers = data => dispatch => {
   const { title, place, contract_type } = data;
-
-  const url = `${domain}api/offers/search/?search=${title}&place=${place}&type=${contract_type}`;
+  const type = ((contract_type === "Tous") ? "" : contract_type);
+  const url = `${domain}api/offers/search/?search=${title}&place=${place}&type=${type}`;
   axios.get(url).then(res => {
     dispatch({
       type: GET_OFFERS,
-      offersList: res.data,
+      offersList: res.data
     });
   });
 };
 
 export const fetchCities = inputPlace => dispatch => {
-  const url = `https://geo.api.gouv.fr/communes?nom=${inputPlace}&fields=nom,codesPostaux&format=json&geometry=centre`
+  const url = `https://geo.api.gouv.fr/communes?nom=${inputPlace}&fields=nom,codesPostaux&format=json&geometry=centre`;
   axios.get(url).then(res => {
     dispatch({
       type: FETCH_CITIES,
-      citiesList: res.data.splice(0,8),
+      citiesList: res.data.splice(0, 8)
     });
   });
 };
