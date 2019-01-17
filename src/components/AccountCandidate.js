51cacdc1dd02625464adb2951c0494d5b5e2daf1
olivier chabot <chabot.oliv@gmail.com>
2019-01-17 18:49:38 +0100

    css see application in space company

--- src/components/AccountCandidate.js
+++ src/components/AccountCandidate.js
@@ -8,6 +8,9 @@ import { getInfoOffersApplication } from "../actions/accountCandidateActions";
 
 import "./css/AccountCandidate.scss";
 
+import iconWaiting from "../images/icons/iconWaiting.png";
+import iconValidate from "../images/icons/iconValidate.png";
+import iconRefuse from "../images/icons/iconRefuse.png";
 
 class AccountCandidate extends Component {
   componentDidMount = () => {
@@ -36,6 +39,26 @@ class AccountCandidate extends Component {
             </ul>
           </div>
           <div className="row tabsContainer p-3">
+            <div className="row align-items-center mb-3 ml-3">
+              <div className="col-2 col-md-auto p-0 text-center">
+                <img src={iconWaiting} className="icon" alt="icone attente" />
+              </div>
+              <div className="col-9 col-md-auto mr-3 align-self-center">
+                <i>En attente</i>
+              </div>
+              <div className="col-2 col-md-auto p-0 text-center">
+                <img src={iconValidate} className="icon" alt="icone validate" />
+              </div>
+              <div className="col-9 col-md-auto mr-3 align-self-center">
+                <i>Validé</i>
+              </div>
+              <div className="col-2 col-md-auto p-0 text-center">
+                <img src={iconRefuse} className="icon" alt="icone refuse" />
+              </div>
+              <div className="col-9 col-md-auto mr-3 align-self-center">
+                <i>Refusé</i>
+              </div>
+            </div>
             <div className="col-12">
               {this.props.listOfCandidatesApplications.map(e => (
                 <OfferCandidate
